114c5eec9eaa44bc2d351594c4dde64ebeaa5655
Dixon <dixon.prado17>
2021-11-07 22:38:37 -0600

    Menu arreglado

--- administrator/cache/t4/d1f65922198b23a13d98b5e9003e9f27-cache-t4-e8d09f9bb06eb1eac19c8c3919f2d008.php
+++ administrator/cache/t4/d1f65922198b23a13d98b5e9003e9f27-cache-t4-e8d09f9bb06eb1eac19c8c3919f2d008.php
@@ -1,4 +1,4 @@
-<?php die("Access Denied"); ?>#x#a:6:{s:6:"layout";s:6446:"<!DOCTYPE html>
+<?php die("Access Denied"); ?>#x#a:6:{s:6:"layout";s:6473:"<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-gb" lang="en-gb" dir="ltr">
 
 <head>
@@ -6,7 +6,7 @@
   {t4post:head}
 
   <!--[if lt IE 9]>
-    <script src="/prueba/media/jui/js/html5.js"></script>
+    <script src="/cuerna/prueba/media/jui/js/html5.js"></script>
   <![endif]-->
   <meta name="viewport"  content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=yes"/>
   <style  type="text/css">
@@ -26,7 +26,7 @@
   
   <div class="t4-offcanvas" data-offcanvas-options='{"modifiers":"right,overlay"}' id="off-canvas-right" role="complementary" style="display:none;">
 	<div class="t4-off-canvas-header">
-	 		  <a href="http://localhost/prueba/" title="Stark.">
+	 		  <a href="http://localhost/cuerna/prueba/" title="Stark.">
 	  	    	  	
 	    	      <span class="site-name">Stark.</span>	      	    
 	  	  </a>
@@ -54,14 +54,14 @@
 
 <div id="t4-menu" class="t4-section  t4-menu  t4-palette-cuerna_menu  t4-sticky">
 <div class="t4-section-inner container"><div class="t4-row row">
-<div class="t4-col mainnav col-sm col-md-6">
+<div class="t4-col mainnav col-sm col-md-7">
 <div class="t4-navbar">
   <jdoc:include type="element" name="megamenu" />
   <jdoc:include type="element" name="offcanvas-toggle" />
 </div>
 
 </div>
-<div class="t4-col col-sm col-md-6">
+<div class="t4-col col-sm col-md-5">
 
 </div>
 </div></div>
@@ -163,4 +163,4 @@
   
 </body>
 </html>
-";s:6:"assets";a:2:{s:6:"script";a:6:{i:0;s:6:"jquery";i:1;s:15:"t4.bootstrap.js";i:2;s:17:"jquery-noconflict";i:3;s:14:"jquery-migrate";i:4;s:13:"html5lightbox";i:5;s:9:"Animation";}s:5:"style";a:4:{i:0;s:13:"font.awesome5";i:1;s:13:"font.awesome4";i:2;s:13:"font.iconmoon";i:3;s:9:"Animation";}}s:8:"_scripts";a:5:{s:83:"/prueba/plugins/system/t4/themes/base/vendors/js-offcanvas/_js/js-offcanvas.pkgd.js";a:2:{s:4:"type";s:15:"text/javascript";s:7:"options";a:0:{}}s:82:"/prueba/plugins/system/t4/themes/base/vendors/bodyscrolllock/bodyScrollLock.min.js";a:2:{s:4:"type";s:15:"text/javascript";s:7:"options";a:0:{}}s:53:"/prueba/plugins/system/t4/themes/base/js/offcanvas.js";a:2:{s:4:"type";s:15:"text/javascript";s:7:"options";a:0:{}}s:41:"/prueba/templates/ja_stark/js/template.js";a:2:{s:4:"type";s:15:"text/javascript";s:7:"options";a:0:{}}s:48:"/prueba/plugins/system/t4/themes/base/js/base.js";a:2:{s:4:"type";s:15:"text/javascript";s:7:"options";a:1:{s:7:"version";s:4:"auto";}}}s:12:"_styleSheets";a:2:{s:80:"/prueba/plugins/system/t4/themes/base/vendors/js-offcanvas/_css/js-offcanvas.css";a:2:{s:4:"type";s:8:"text/css";s:7:"options";a:0:{}}s:55:"templates/ja_stark/fonts/LouisGeorgeCafe/stylesheet.css";a:2:{s:4:"type";s:8:"text/css";s:7:"options";a:0:{}}}s:6:"_links";a:2:{s:42:"/prueba/index.php?format=feed&amp;type=rss";a:3:{s:8:"relation";s:9:"alternate";s:7:"relType";s:3:"rel";s:7:"attribs";a:2:{s:4:"type";s:19:"application/rss+xml";s:5:"title";s:7:"RSS 2.0";}}s:43:"/prueba/index.php?format=feed&amp;type=atom";a:3:{s:8:"relation";s:9:"alternate";s:7:"relType";s:3:"rel";s:7:"attribs";a:2:{s:4:"type";s:20:"application/atom+xml";s:5:"title";s:8:"Atom 1.0";}}}s:5:"_file";s:51:"C:\xampp\htdocs\prueba\templates/ja_stark/index.php";}
+";s:6:"assets";a:2:{s:6:"script";a:6:{i:0;s:6:"jquery";i:1;s:15:"t4.bootstrap.js";i:2;s:17:"jquery-noconflict";i:3;s:14:"jquery-migrate";i:4;s:13:"html5lightbox";i:5;s:9:"Animation";}s:5:"style";a:4:{i:0;s:13:"font.awesome5";i:1;s:13:"font.awesome4";i:2;s:13:"font.iconmoon";i:3;s:9:"Animation";}}s:8:"_scripts";a:5:{s:90:"/cuerna/prueba/plugins/system/t4/themes/base/vendors/js-offcanvas/_js/js-offcanvas.pkgd.js";a:2:{s:4:"type";s:15:"text/javascript";s:7:"options";a:0:{}}s:89:"/cuerna/prueba/plugins/system/t4/themes/base/vendors/bodyscrolllock/bodyScrollLock.min.js";a:2:{s:4:"type";s:15:"text/javascript";s:7:"options";a:0:{}}s:60:"/cuerna/prueba/plugins/system/t4/themes/base/js/offcanvas.js";a:2:{s:4:"type";s:15:"text/javascript";s:7:"options";a:0:{}}s:48:"/cuerna/prueba/templates/ja_stark/js/template.js";a:2:{s:4:"type";s:15:"text/javascript";s:7:"options";a:0:{}}s:55:"/cuerna/prueba/plugins/system/t4/themes/base/js/base.js";a:2:{s:4:"type";s:15:"text/javascript";s:7:"options";a:1:{s:7:"version";s:4:"auto";}}}s:12:"_styleSheets";a:2:{s:87:"/cuerna/prueba/plugins/system/t4/themes/base/vendors/js-offcanvas/_css/js-offcanvas.css";a:2:{s:4:"type";s:8:"text/css";s:7:"options";a:0:{}}s:55:"templates/ja_stark/fonts/LouisGeorgeCafe/stylesheet.css";a:2:{s:4:"type";s:8:"text/css";s:7:"options";a:0:{}}}s:6:"_links";a:2:{s:49:"/cuerna/prueba/index.php?format=feed&amp;type=rss";a:3:{s:8:"relation";s:9:"alternate";s:7:"relType";s:3:"rel";s:7:"attribs";a:2:{s:4:"type";s:19:"application/rss+xml";s:5:"title";s:7:"RSS 2.0";}}s:50:"/cuerna/prueba/index.php?format=feed&amp;type=atom";a:3:{s:8:"relation";s:9:"alternate";s:7:"relType";s:3:"rel";s:7:"attribs";a:2:{s:4:"type";s:20:"application/atom+xml";s:5:"title";s:8:"Atom 1.0";}}}s:5:"_file";s:58:"C:\xampp\htdocs\cuerna\prueba\templates/ja_stark/index.php";}
